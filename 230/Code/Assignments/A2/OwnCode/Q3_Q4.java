f08cbb1c491c6774b50acb5f8f9c3c4c2e84d354
Mingyang Li <53138432+Mingyang-Li@users.noreply.github.com>
2020-09-22 21:03:21 +1200

    lots of errors

--- 230/Code/Assignments/A2/OwnCode/Q3_Q4.java
+++ 230/Code/Assignments/A2/OwnCode/Q3_Q4.java
@@ -1,10 +1,20 @@
 class DynamicRectangleShape extends RectangleShape{
     public DynamicRectangleShape(int x, int y, int deltaX, int deltaY, int width, int height){
-
+        super( x, y, deltaX, deltaY, width, height);
+        @Override void paint(Painter painter){
+            super.setColor(Color Color.red);
+        }
     }
 
     public DynamicRectangleShape(int x, int y, int deltaX, int deltaY, int width, int height, Color color) {
-        
+        super( x, y, deltaX, deltaY, width, height);
+        @Override void paint(Painter painter){
+            super.setColor(Color color);
+        }
+    }
+
+    @Override move(int width, int height){
+        super.move(width, height);
     }
 
 }
