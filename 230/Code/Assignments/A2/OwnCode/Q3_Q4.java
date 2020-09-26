2ae72db427346efc18777d78c128f2cd08d92713
Mingyang Li <53138432+Mingyang-Li@users.noreply.github.com>
2020-09-26 16:25:29 +1200

    Update Q3_Q4.java

--- 230/Code/Assignments/A2/OwnCode/Q3_Q4.java
+++ 230/Code/Assignments/A2/OwnCode/Q3_Q4.java
@@ -1,17 +1,31 @@
 class DynamicRectangleShape extends RectangleShape {
+    private Color color;
+    private Boolean bounced;
     public DynamicRectangleShape(int x, int y, int deltaX, int deltaY, int width, int height){
         super(x, y, deltaX, deltaY, width, height);
+        this.color = red; // or should it be equal to Color.red ? 
     }
 
     public DynamicRectangleShape(int x, int y, int deltaX, int deltaY, int width, int height, Color color) {
         super(x, y, deltaX, deltaY, width, height);
+        this.color = color; // not sure if I'm assigning color correctly
+    }
+    @Override public void move(int width, int height){
+        /*
+        if (the shape bounced){
+            bounced = true;
+        }
+        */
+        //super.move(super.width, super.height);
     }
     @Override public void paint(Painter painter){
-        super.paint(painter);
-    }
-
-    @Override public void move(int width, int height){
-        super.move(super.width(), super.height());
+        /*
+        if (color is not provide in constructor){
+            paint it red 
+            super.paint(Color.red); ??? - not sure if this is how you pass in colors 
+        } else {
+            paint it with the color specified 
+        }
+         */
     }
 }
-
