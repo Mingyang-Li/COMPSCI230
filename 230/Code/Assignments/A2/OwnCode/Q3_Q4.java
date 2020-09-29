16b609623f9498206ace2f6bc03be286282cc1a5
Mingyang Li <53138432+Mingyang-Li@users.noreply.github.com>
2020-09-29 14:43:47 +1300

    Update Q3_Q4.java

--- 230/Code/Assignments/A2/OwnCode/Q3_Q4.java
+++ 230/Code/Assignments/A2/OwnCode/Q3_Q4.java
@@ -1,6 +1,9 @@
+import java.awt.Color;
+
 class DynamicRectangleShape extends RectangleShape {
     private Color color;
     private Boolean bounced;
+    private String side;
     public DynamicRectangleShape(int x, int y, int deltaX, int deltaY, int width, int height){
         super(x, y, deltaX, deltaY, width, height);
         this.color = Color.red; 
@@ -8,24 +11,41 @@ class DynamicRectangleShape extends RectangleShape {
 
     public DynamicRectangleShape(int x, int y, int deltaX, int deltaY, int width, int height, Color color) {
         super(x, y, deltaX, deltaY, width, height);
-        this.color = color; 
+        this.color = color; // not sure if I'm assigning color correctly
     }
     @Override public void move(int width, int height){
-        /*
-        if (the shape bounced){
-            bounced = true;
-        }
-        */
-        //super.move(super.width, super.height);
+        super.move(width, height);
+        if (_x == 0){
+            side = "horizontal";
+        } 
+        if (_y == 0){
+            side = "vertical";
+        } 
+        // top left 
+        if (_x == 0 && _y == 0){
+            side = "vertical";
+        } 
+        // top right 
+        if (_x == _width && _y == 0){
+            side = "vertical";
+        } 
+        // bottom left 
+        if (_x == 0 && _y == _height){
+            side = "vertical";
+        } 
+        // bottom right 
+        if (_x == _width && _y == _height){
+            side = "vertical";
+        } 
     }
-    @Override public void paint(Painter painter){
-        /*
-        if (color is not provide in constructor){
-            paint it red 
-            super.paint(Color.red); 
+    @Override 
+    public void paint(Painter painter){
+        if (this.side == "horizontal"){
+            // paint itself as a solid shape
+            painter.fillRect(_x, _y, _width, _height);
         } else {
-            paint it with the color specified 
+            // dont paint, use superclass paint 
+            super.paint(painter);
         }
-         */
     }
 }
