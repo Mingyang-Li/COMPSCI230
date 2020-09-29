86265220878a4cc211d0ee23ee456e5096c9dc44
Mingyang Li <53138432+Mingyang-Li@users.noreply.github.com>
2020-09-30 00:06:46 +1300

    correct ans

--- 230/Code/Assignments/A2/OwnCode/Q3_Q4.java
+++ 230/Code/Assignments/A2/OwnCode/Q3_Q4.java
@@ -2,50 +2,74 @@ import java.awt.Color;
 
 class DynamicRectangleShape extends RectangleShape {
     private Color color;
-    private Boolean bounced;
-    private String side;
+    private Boolean filled = true;
     public DynamicRectangleShape(int x, int y, int deltaX, int deltaY, int width, int height){
         super(x, y, deltaX, deltaY, width, height);
-        this.color = Color.red; 
+        this.color = Color.RED; 
     }
 
     public DynamicRectangleShape(int x, int y, int deltaX, int deltaY, int width, int height, Color color) {
         super(x, y, deltaX, deltaY, width, height);
-        this.color = color; // not sure if I'm assigning color correctly
+        this.color = color; 
     }
-    @Override public void move(int width, int height){
+
+    @Override
+    public void move(int width, int height) {
         super.move(width, height);
-        if (_x == 0){
-            side = "horizontal";
-        } 
-        if (_y == 0){
-            side = "vertical";
-        } 
-        // top left 
-        if (_x == 0 && _y == 0){
-            side = "vertical";
-        } 
-        // top right 
-        if (_x == _width && _y == 0){
-            side = "vertical";
-        } 
-        // bottom left 
-        if (_x == 0 && _y == _height){
-            side = "vertical";
-        } 
-        // bottom right 
-        if (_x == _width && _y == _height){
-            side = "vertical";
-        } 
+        if (_y + this.height() == height || _y <= 0) {
+            filled = false;
+        }
+        if (_x <= 0 || _x + this.width() >= width) {
+            filled = true;
+        }
     }
+
     @Override 
     public void paint(Painter painter){
-        if (this.side == "horizontal"){
-            // paint itself as a solid shape
+        if (filled) {
+            painter.setColor(this.color);
             painter.fillRect(_x, _y, _width, _height);
+            painter.setColor(Color.BLACK);
         } else {
-            // dont paint, use superclass paint 
-            super.paint(painter);
+            // paint the shape anyway regardless color
+            painter.drawRect(_x, _y, _width, _height);
+        }   
+    }
+}import java.awt.Color;
+
+class DynamicRectangleShape extends RectangleShape {
+    private Color color;
+    private Boolean filled = true;
+    public DynamicRectangleShape(int x, int y, int deltaX, int deltaY, int width, int height){
+        super(x, y, deltaX, deltaY, width, height);
+        this.color = Color.RED; 
+    }
+
+    public DynamicRectangleShape(int x, int y, int deltaX, int deltaY, int width, int height, Color color) {
+        super(x, y, deltaX, deltaY, width, height);
+        this.color = color; 
+    }
+
+    @Override
+    public void move(int width, int height) {
+        super.move(width, height);
+        if (_y + this.height() == height || _y <= 0) {
+            filled = false;
         }
+        if (_x <= 0 || _x + this.width() >= width) {
+            filled = true;
+        }
+    }
+
+    @Override 
+    public void paint(Painter painter){
+        if (filled) {
+            painter.setColor(this.color);
+            painter.fillRect(_x, _y, _width, _height);
+            painter.setColor(Color.BLACK);
+        } else {
+            // paint the shape anyway regardless color
+            painter.drawRect(_x, _y, _width, _height);
+        }   
     }
 }
